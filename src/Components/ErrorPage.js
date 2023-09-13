097462ea309632e0f3eb14d487af4ed63ff74373
Жамолиддин <mcjik@icloud.com>
2023-09-13 12:04:04 +0500

    Updates

--- src/Components/ErrorPage.js
+++
@@ -1,9 +0,0 @@
-const ErrorPage = () => {
-  return (
-    <>
-      <h1>Sorry an Error has occured</h1>
-    </>
-  );
-};
-
-export default ErrorPage;
--- src/Components/SuccessPage.js
+++
@@ -1,5 +0,0 @@
-const SuccessPage = () => {
-  return <h1>Success</h1>;
-};
-
-export default SuccessPage;
